24a930720d09add561c1acb344a50f75038c7d17
kandanaoki <84160141+kandanaoki@users.noreply.github.com>
2021-05-28 16:59:17 +0900

    「ログイン状態のユーザーであっても、他のユーザーのプロトタイプ編集ページのURLを直接入力して遷移しようとすると、トップページにリダイレクトされること」の修正と表示の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,7 +26,7 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    unless user_signed_in?
+    unless user_signed_in? && @prototype.user_id == current_user.id
       redirect_to action: :index
     end
   end
